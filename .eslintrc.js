c24a3a3593ced295f7ff7e9680748e72e74f180a
Rich Gwozdz <rgwozdz@esri.com>
2023-11-08 06:43:36 -0800

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-function-return-type': ['warning'],
+        '@typescript-eslint/explicit-function-return-type': ['warn'],
         "@typescript-eslint/typedef": [
             "error",
             {
